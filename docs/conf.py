85bca4ea30530d723946f3f64f09c6c8122213c3
Alan Taranti <alan.taranti@gmail.com>
2019-09-19 22:22:10 -0400

    adicionado napoleon em sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,8 @@ release = sdk.VERSAO
 # ones.
 extensions = [
     "sphinx_rtd_theme",
-    "sphinx.ext.autodoc"
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
