7c3c45f917d473f03f2e57dc7ab15aee6c1b474d
Alan Taranti <alan.taranti@gmail.com>
2019-09-23 12:16:09 -0400

    alterado formato documentação para o numpy e adicionado exemplos em lojas

--- lomapy/lojas.py
+++ lomapy/lojas.py
@@ -8,17 +8,57 @@ from lomapy.recursos.rotas import rotas_loja
 
 
 def obter_todas(categoria_id: int = None, possui_oferta: bool = None) -> dict:
-    """
-    Lista os parceiros e lojistas da Lomadee
+    """Lista os parceiros e lojistas da Lomadee
 
-    :param categoria_id: ID de categoria. Utilize esse parâmetro para filtrar lojas que possuem ofertas de uma determinada categoria
-    :type categoria_id: int, optional
+    Parameters
+    ----------
+    categoria_id: int, optional
+        ID de categoria. Utilize esse parâmetro para filtrar lojas que possuem ofertas de uma determinada categoria
 
-    :param possui_oferta: Quando "true" retorna apenas lojas que possuem ofertas.
-    :type possui_oferta: bool, optional
+    possui_oferta: bool, optional
+        Quando "true" retorna apenas lojas que possuem ofertas.
 
-    :return: Lojas
-    :rtype: dict
+    Returns
+    -------
+    dict
+        Retorna as Lojas encontradas
+
+    Examples
+    --------
+    >>> import lomapy
+    >>> from pprint import pprint
+    >>> app_token = "MEU_APP_TOKEN"
+    >>> source_id = "MEU_SOURCE_ID"
+    >>> lomapy.autenticar(app_token, source_id)
+    >>> resposta = lomapy.lojas.obter_todas()
+    >>> # Atenção: Mostrado apenas duas lojas, por questão de espaço
+    >>> pprint(resposta)  # doctest: +NORMALIZE_WHITESPACE
+    {'pagination': {'page': 1, 'size': 178, 'totalPage': 1, 'totalSize': 178},
+     'requestInfo': {'generatedDate': None, 'message': 'SUCCESS', 'status': 'OK'},
+     'stores': [{'events': [{'commission': 0.064,
+                             'event': 'Vendas - Hang Loose',
+                             'eventType': 'Sale',
+                             'fixedCommission': False}],
+                 'hasOffer': 0,
+                 'id': 5695,
+                 'link': 'https://developer.lomadee.com/redir/validation/?sourceId=MEU_SOURCE_ID&appToken=MEU_APP_TOKEN',
+                 'maxCommission': 6.4,
+                 'name': 'Hang Loose',
+                 'thumbnail': 'https://www.lomadee.com/programas/BR/5695/logo_115x76.png'},
+                {'events': [{'commission': 0.0354,
+                             'event': 'Cozinha, Ar condicionado, Ferramentas',
+                             'eventType': 'Sale',
+                             'fixedCommission': False},
+                            {'commission': 0.0354,
+                             'event': 'Casa e Móveis',
+                             'eventType': 'Sale',
+                             'fixedCommission': False}],
+                 'hasOffer': 0,
+                 'id': 5800,
+                 'link': 'https://developer.lomadee.com/redir/validation/?sourceId=MEU_SOURCE_ID&appToken=MEU_APP_TOKEN',
+                 'maxCommission': 3.54,
+                 'name': 'Mega Mamute',
+                 'thumbnail': 'https://www.lomadee.com/vitrine/logo590367.gif'}]}
     """
     parametros = {}
 
